c4d74cfab2f8d79ad06c343b9b87a79581d7ff98
Alvin Febriando <alvinfebriando@outlook.com>
2023-01-22 12:00:50 +0700

    Add survey question

--- src/page/question.tsx
+++ src/page/question.tsx
@@ -1,15 +1,18 @@
-import { Checkbox, Stack } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import { Model, StylesManager } from 'survey-core';
+import 'survey-core/modern.min.css';
+import { Survey } from 'survey-react-ui';
+import surveyJson from '../data/survey.json';
+
+StylesManager.applyTheme('modern');
 
 const Question = () => {
-  return (
-    <>
-      <Stack spacing={5} direction='row'>
-        <Checkbox isDisabled>Checkbox</Checkbox>
-        <Checkbox isDisabled defaultChecked>
-          Checkbox
-        </Checkbox>
-      </Stack>
-    </>
-  );
+  const survey = new Model(surveyJson);
+  const alertResult = useCallback((sender: Model) => {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+  }, []);
+  survey.onComplete.add(alertResult);
+  return <Survey model={survey} />;
 };
 export default Question;
