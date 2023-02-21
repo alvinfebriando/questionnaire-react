a1340faa4eaacb67a39518ec5a0b593d48ead475
Alvin Febriando <alvinfebriando@outlook.com>
2023-02-21 08:24:31 +0700

    Add copy clipboard button

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,19 +1,40 @@
-import { Card, CardBody, Flex, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  CardBody,
+  Flex,
+  Stack,
+  Text,
+  useClipboard,
+} from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 type Props = {
   scores: string;
 };
 
-const Result = (props: Props) => (
-  <Card>
-    <CardBody>
-      <Flex>
-        <Text style={{ whiteSpace: 'pre-wrap' }}>
-          {props.scores ? props.scores : 'Result:'}
-        </Text>
-      </Flex>
-    </CardBody>
-  </Card>
-);
+const Result = (props: Props) => {
+  const { onCopy, value, setValue, hasCopied } = useClipboard('');
+  useEffect(() => {
+    setValue(props.scores);
+  });
+
+  return (
+    <Card>
+      <CardBody>
+        <Flex>
+          <Stack>
+            <Button onClick={onCopy} width={'5rem'}>
+              {hasCopied ? 'Copied!' : 'Copy'}
+            </Button>
+            <Text style={{ whiteSpace: 'pre-wrap' }}>
+              {props.scores ? props.scores : 'Result:'}
+            </Text>
+          </Stack>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default Result;
